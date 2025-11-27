774cd16f9a5f08290c00a4761961d59ee3d825fa
Petro Lungu <polo200875@gmail.com>
2025-11-27 15:18:30 +0100

    added configuration for deploy

--- homework25/smile-vote-app/src/App.jsx
+++ homework25/smile-vote-app/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react' 
-import smile1 from './assets/smile1.svg'
-import smile2 from './assets/smile2.svg'
-import smile3 from './assets/smile3.svg'
-import smile4 from './assets/smile4.svg'
-import smile5 from './assets/smile5.svg'
-import './App.css'
+import React, { Component } from 'react';
+import smile1 from './assets/smile1.svg';
+import smile2 from './assets/smile2.svg';
+import smile3 from './assets/smile3.svg';
+import smile4 from './assets/smile4.svg';
+import smile5 from './assets/smile5.svg';
+import './App.css';
 
 const smileMap = {
   smile1: smile1,
@@ -14,24 +14,41 @@ const smileMap = {
   smile5: smile5,
 };
 
-function App() {
+const SMILE_DATA = [
+  { id: 'smile1', alt: 'smile1' },
+  { id: 'smile2', alt: 'smile2' },
+  { id: 'smile3', alt: 'smile3' },
+  { id: 'smile4', alt: 'smile4' },
+  { id: 'smile5', alt: 'smile5' },
+];
 
-  const initialVotes = { smile1: 0, smile2: 0, smile3: 0, smile4: 0, smile5: 0 };
-  const initialResults = { maxCount: 0, winners: [] };
+const initialVotes = { smile1: 0, smile2: 0, smile3: 0, smile4: 0, smile5: 0 };
+const initialResults = { maxCount: 0, winners: [] };
 
-  const [votes, setVotes] = useState(initialVotes);
-  const [results, setResults] = useState(initialResults);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: initialVotes,
+      results: initialResults,
+    };
+    this.handleVote = this.handleVote.bind(this);
+    this.handleResult = this.handleResult.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
 
-
-  const handleVote = (smileId) => {
-    setVotes(prevVotes => ({
-      ...prevVotes,
-      [smileId]: prevVotes[smileId] + 1
+  handleVote(smileId) {
+    this.setState(prevState => ({
+      votes: {
+        ...prevState.votes,
+        [smileId]: prevState.votes[smileId] + 1
+      },
+      results: initialResults,
     }));
-    setResults(initialResults); 
-  };
+  }
 
-    const handleResult = () => {
+  handleResult() {
+    const { votes } = this.state;
     const voteValues = Object.values(votes);
     const maxVoteCount = voteValues.length > 0 ? Math.max(...voteValues) : 0;
     
@@ -39,83 +56,76 @@ function App() {
     if (maxVoteCount > 0) {
       winners = Object.keys(votes).filter(key => votes[key] === maxVoteCount);
     }
-  
-    setResults({
-      maxCount: maxVoteCount,
-      winners: winners
+
+    this.setState({
+      results: {
+        maxCount: maxVoteCount,
+        winners: winners,
+      }
     });
   }
 
-   const handleReset = () => {
-    setVotes(initialVotes);
-    setResults(initialResults);
-   };
-    
-  const { maxCount, winners } = results;
+  handleReset() {
+    this.setState({
+      votes: initialVotes,
+      results: initialResults,
+    });
+  }
 
-  return (
-    <>
+  render() {
+    const { votes, results } = this.state;
+    const { maxCount, winners } = results;
+
+    return (
+      <>
         <h1>Голосування за найкращий смайлик</h1>
         <div className="smile-container">
-        <div className="card">
-          <button onClick={() => handleVote('smile1')}><img src={smile1} className="logo" alt="smile1" />
-            count is {votes.smile1}
-          </button>
-        </div>
-        <div className="card">
-          <button onClick={() => handleVote('smile2')}><img src={smile2} className="logo" alt="smile2" />
-            count is {votes.smile2}
-          </button>
-        </div>
-        <div className="card">
-          <button onClick={() => handleVote('smile3')}> <img src={smile3} className="logo" alt="smile3" />
-            count is {votes.smile3}
-          </button>
-        </div>
-        <div className="card">
-          <button onClick={() => handleVote('smile4')}><img src={smile4} className="logo" alt="smile4" />
-            count is {votes.smile4}
-          </button>
-        </div>
-        <div className="card">
-          <button onClick={() => handleVote('smile5')}><img src={smile5} className="logo" alt="smile5" />
-            count is {votes.smile5} </button>
-        </div>
-
-      </div>
-
-      <div className="card">
-        <button onClick={handleResult}>
-          Showresults 🏆
-        </button>
-        <button onClick={handleReset}>
-          Reset 🔄
-        </button>
-      </div>
-   
-      <div className="card results-display">
-        <h1>Результати голосування:</h1>
-        
-        {winners.length > 0 ? (
-          <div>
-          <p>
-  {winners.length > 1 ? 'Переможці' : 'Переможець'} (Кількість голосів: {maxCount}):
-            </p>
-            <div className="smile-container">
-              
-              {winners.map(winnerId => (
-                <div key={winnerId} className="winner-item">
-                   <img src={smileMap[winnerId]} className="logo" alt={winnerId} />
-                </div>
-              ))}
+          {SMILE_DATA.map((smile) => (
+            <div className="card" key={smile.id}>
+              <button onClick={() => this.handleVote(smile.id)}>
+                <img 
+                  src={smileMap[smile.id]} 
+                  className="logo" 
+                  alt={smile.alt} 
+                />
+                count is {votes[smile.id]}
+              </button>
             </div>
-          </div>
-        ) : (
-          <p>Натисніть "Показати результати" або проголосуйте, щоб побачити переможця.</p>
-        )}
-      </div>
-    </>
-  )
+          ))}
+        </div>
+
+        <div className="card">
+          <button onClick={this.handleResult}>
+            Showresults 🏆
+          </button>
+          <button onClick={this.handleReset}>
+            Reset 🔄
+          </button>
+        </div>
+      
+        <div className="card results-display">
+          <h1>Результати голосування:</h1>
+          
+          {winners.length > 0 ? (
+            <div>
+              <p>
+                {winners.length > 1 ? 'Переможці' : 'Переможець'} (Кількість голосів: {maxCount}):
+              </p>
+              <div className="smile-container">
+                {winners.map(winnerId => (
+                  <div key={winnerId} className="winner-item">
+                    <img src={smileMap[winnerId]} className="logo" alt={winnerId} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <p>Натисніть "Показати результати" або проголосуйте, щоб побачити переможця.</p>
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
